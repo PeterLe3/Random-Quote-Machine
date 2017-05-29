6503b85605e2330faaea6bc20c51d39b587c6a97
Peter Le <Peterle3@csus.edu>
2017-05-28 20:55:35 -0700

    add more colors and author

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function() {
   var quoteBlock = $('#text');
+  var author = $('#text2');
+  var colors = ["#ADD8E6", " #FF7F50" ," #3CB371" ,"#BC8F8F" ,"#A52A2A" ," #F0E68C" ," #FF6347"];
+  var test = 0;
+  var i = 0;
+
   $("#quote").click(function() {
   
       
@@ -17,28 +22,30 @@ $(document).ready(function() {
          success: function(data){
 
          console.log("Sucess!", data.quote);
-         quoteBlock.text(data.quote);
+  
+         i = dostuff(test, i);
+         test = i;
+
+         
+         $("body").css("background-color", colors[i]);
+         quoteBlock.text('"' +data.quote + '"');
+         author.text("- " +data.author);
+         author.css("font-size", "1em");
          }
            
       });
   });
    $("#quote2").click(function() {
-  
-   
-       //document.getElementById("text").innerHTML = "Paragraph changed!";
-          var test = "";
-          var arr = quoteBlock.text().split(" ");
-          console.log(arr);
-         
-          for(var i = 0; i < arr.length; i++) {
-            // test+='%20';
-            test+=arr[i] + ' ';
-         
-          }
-          console.log(test);
-         window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(test));       
+         var tweet  = quoteBlock.text();
+         window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(tweet));       
 });
-    
+
+   function dostuff(test, i) {
+     while(test == i ) {
+           var i = Math.floor(Math.random() * 7);
+         }
+         return i;
+   }
       
   });
 
